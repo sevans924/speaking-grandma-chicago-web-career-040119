b45d506fb4dbd7781812742ffa4cf82244e53121
Sarah Elzinga <sevans924@gmail.com>
2019-03-20 19:02:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-elsif phrase != phrase.downcase
-    return 'NO, NOT SINCE 1938!'
-  else
+elsif phrase != phrase.upcase
     return 'HUH?! SPEAK UP, SONNY!'
+  else
+    return 'NO, NOT SINCE 1938!'
   end
 end
 # Write a speak_to_grandma method.
